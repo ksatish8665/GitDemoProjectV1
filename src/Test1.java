ad36ed584e14e9ef41195c68642a32cfb3d4dd51
ksatish8665 <109174747+ksatish8665@users.noreply.github.com>
2022-07-15 16:57:49 +0530

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,15 @@
 
 public class Test1 {
+	
+	public void display(){
+	System.out.println("Automation Selenium");	
+	}
+	public void addmethod(){
+		int x=100;
+		int y=200;
+		int count = x+y;
+		System.out.println(count);
+	}
 
 	public static void main(String[] args) {
 		String hungry = "yes";
@@ -10,8 +20,11 @@ public class Test1 {
 		System.out.println("eat ........somthing");
 		
 	else {
-		System.out.println("do......work");
+		System.out.println("do......work");	
 	}
+		Test1 t = new Test1();
+		t.display();
+		t.addmethod();
 	}
 
 }
