b897c79c890d09060db12a13f7c3aa7318d99342
satish <satishkotla36@gmail.com>
2022-07-18 16:35:19 +0530

    Commited Test2 File

---
+++ src/Test2.java
@@ -0,0 +1,30 @@
+
+public class Test2 {
+	
+	public void display(){
+	System.out.println("Automation Selenium");	
+	}
+	public void addmethod(){
+		int x=100;
+		int y=200;
+		int count = x+y;
+		System.out.println(count);
+	}
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		
+		
+	if (hungry == "yes")
+		
+		System.out.println("eat ........somthing");
+		
+	else {
+		System.out.println("do......work");	
+	}
+		Test2 t = new Test2();
+		t.display();
+		t.addmethod();
+	}
+
+}
